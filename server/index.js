3256f0e8bae555866689f3c9f71579ab1edb608f
Shivam <shivam20020507@gmail.com>
2024-09-06 17:52:20 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://study-notion1-red.vercel.app",
 		credentials:true,
 	})
 )
