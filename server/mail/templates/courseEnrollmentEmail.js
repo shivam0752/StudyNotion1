88d702f4a305e353589da05ef7dff2f4dd65f41a
Shivam <shivam20020507@gmail.com>
2024-09-06 18:26:51 +0530

    Update courseEnrollmentEmail.js

--- server/mail/templates/courseEnrollmentEmail.js
+++ server/mail/templates/courseEnrollmentEmail.js
@@ -67,7 +67,7 @@ exports.courseEnrollmentEmail = (courseName, name) => {
   
   <body>
       <div class="container">
-          <a href="https://studynotion-edtech-project.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
+          <a href="https://study-notion1-red.vercel.app"><img class="logo" src="https://i.ibb.co/7Xyj3PC/logo.png"
                   alt="StudyNotion Logo"></a>
           <div class="message">Course Registration Confirmation</div>
           <div class="body">
